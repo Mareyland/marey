dde48acc6d3c2c296edf2dd7377cf45e4e885ccf
marcel <marcel.hajek7@gmail.com>
2026-06-07 06:21:01 +0200

    ...

--- include/marey/Core/Kernel.h
+++ include/marey/Core/Kernel.h
@@ -16,8 +16,6 @@ public:
     void run();
 
 private:
-    void greedBack();
-
     ModuleManager moduleManager{};
 };
 }
--- lib/Core/Kernel.cpp
+++ lib/Core/Kernel.cpp
@@ -3,16 +3,8 @@
 #include <marey/Core/CoreModule.h>
 #include <marey/Io/IoManager.h>
 
-using marey::Io::IoManager;
-
 namespace marey::Core {
 void Kernel::run() {
-    greedBack();
-
     moduleManager.run(CoreModule{});
-}
-
-void Kernel::greedBack() {
-    IoManager::print("Hello Hollow :3");
 }
 }
